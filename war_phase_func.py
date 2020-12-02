2448f068a9ace4df4bff2f2d4bb9e79a5f6aaa12
brutatoasta <60847718+brutatoasta@users.noreply.github.com>
2020-12-02 10:58:12 +0800

    Create war_phase_func.py

---
+++ war_phase_func.py
@@ -0,0 +1,30 @@
+import random
+def choose_start(ls_player_id):
+    return random.choice(ls_player_id)
+    
+def attack():
+    shot_bool = False
+    my_turn_bool = True
+    while shot_bool == False:
+        shot_str = input("Attack coordinates? e.g. A0 ")
+        if check_valid_point(shot_str, "Shot"): #check if string is garbage
+            shot = Point(shot[0].upper(), shot[1]) #save the shot as Point object
+            if shot in shot_list:
+                print("You have taken this shot before! Choose another!") 
+                shot_bool = False
+            else:
+                print("Shooting...")
+                #sleep(3)
+                shot_bool = True
+        else:
+            shot_bool = False
+   
+    if shot in enemy_all_ships_points_list:
+        print("Shot to {} was succesful!".format(str(shot.x + shot.y)))
+        shot_list.append(shot)
+        #check if any enemy ships were sunk
+        #check if all enemy ships sunk
+    else:
+        print("You missed.")
+        shot_list.append(shot)
+    myturn_bool = False
