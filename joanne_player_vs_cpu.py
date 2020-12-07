6c9de53efe9af9e136ed3817b21f078197732c88
brutatoasta <60847718+brutatoasta@users.noreply.github.com>
2020-12-07 22:30:47 +0800

    print statement for CPU and P1 scores

--- joanne_player_vs_cpu.py
+++ joanne_player_vs_cpu.py
@@ -438,6 +438,8 @@ def main():
         #CPU checks for game end
         CPU_score = check_score(P1_ship_dict)
         P1_score = check_score(CPU_ship_dict)
+        print("CPU_score:", CPU_score)
+        print("P1_score": P1_score)
         if CPU_score == 5:
             print("You lost!")
             game_not_over = False # what if they are 5 at the same time
